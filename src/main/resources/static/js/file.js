edb4b5db3f1fa35983264b02b3ca01d36b2c3eed
Kamronbek <kamronbekmahmudxonov@gmail.com>
2021-09-30 11:47:43 +0500

    Changes by Muhriddin aka

--- src/main/resources/static/js/file.js
+++ src/main/resources/static/js/file.js
@@ -4,7 +4,7 @@ let str, file;
 uploadField.onchange = function () {
     file = this.files[0];
     str = file.name.toLowerCase();
-    if (str.endsWith('.png') || str.endsWith('.jpg')) {
+    if (str.toLowerCase().endsWith('.png') || str.toLowerCase().endsWith('.jpg')|| str.toLowerCase().endsWith('.jpeg')|| str.toLowerCase().endsWith('.jfif')) {
         if (file.size > 5000000) {
             alert("Fayl xajmi 5mb katta, iltmos kichikroq hajimli rasm kiriting!!!");
             this.value = "";
