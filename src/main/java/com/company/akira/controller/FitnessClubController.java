f679775ed8cfdd64c6ca8b36c16f8a87bdeee50a
Kamronbek <kamronbekmahmudxonov@gmail.com>
2021-10-02 13:49:59 +0500

    changed post action in trenajor.html

--- src/main/java/com/company/akira/controller/FitnessClubController.java
+++ src/main/java/com/company/akira/controller/FitnessClubController.java
@@ -1,11 +1,7 @@
 package com.company.akira.controller;
 
-import com.company.akira.model.Actor;
-import com.company.akira.model.AutoService;
 import com.company.akira.model.FitnessClub;
-import com.company.akira.repository.ActorRepository;
 import com.company.akira.repository.FitnessClubRepository;
-import com.company.akira.utl.Const;
 import com.company.akira.utl.Utils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,9 +10,6 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Controller
 @RequestMapping("/catalog/fitnes-club")
@@ -47,7 +40,6 @@ public class FitnessClubController {
             redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
             return "redirect:uploadStatus";
         }
-
         try {
             // Get the file and save it somewhere
             Utils.saveImage(file,service);
@@ -56,7 +48,8 @@ public class FitnessClubController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        repo.save(service);
+        FitnessClub save = repo.save(service);
+        System.out.println(save);
         return "redirect:/uploadStatus";
     }
 }
