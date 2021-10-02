ef41dcf6d25d0c59b58aab9a1d725106ad9ea6af
Kamronbek <kamronbekmahmudxonov@gmail.com>
2021-10-02 12:29:33 +0500

    edited crop.js file

--- src/main/resources/static/js/crop.js
+++ src/main/resources/static/js/crop.js
@@ -1,8 +1,8 @@
-function crop() {
-    var qiymat = document.getElementById("srcc").value;
-    var need = qiymat.indexOff("src") + 5;
-    var need = need + 44;
-    var tayyor = qiymat.substring(need, need2);
-    document.getElementById("tayy").value = tayyor
-//<div style="position:relative;overflow:hidden;"><a href="https://yandex.uz/maps/10335/tashkent/?utm_medium=mapframe&utm_source=maps" style="color:#eee;font-size:12px;position:absolute;top:0px;">Ташкент</a><a href="https://yandex.uz/maps/10335/tashkent/?from=api-maps&ll=69.276492%2C41.300569&origin=jsapi_2_1_79&pt=69.27649199999999%2C41.30056900002783~69.277005%2C41.300887~69.27532%2C41.300043~69.27682%2C41.302318~69.274598%2C41.299005~69.277831%2C41.302759~69.279006%2C41.302065~69.274609%2C41.298497~69.275357%2C41.297741~69.273062%2C41.299128&utm_medium=mapframe&utm_source=maps&z=16" style="color:#eee;font-size:12px;position:absolute;top:14px;">Яндекс.Карты — транспорт, навигация, поиск мест</a><iframe src="https://yandex.uz/map-widget/v1/-/CCUqQOdE8B" width="560" height="400" frameborder="1" allowfullscreen="true" style="position:relative;"></iframe></div>
-}
+var elm = document.getElementById('map_url');
+elm.addEventListener('change', function (e) {
+    var s = this.value, i = s.indexOf("src") + 5;
+    if (i !== -1) {
+        s = s.substring(i, (i + 44));
+        this.value = s;
+    }
+});
--- src/main/resources/static/js/file.js
+++ src/main/resources/static/js/file.js
@@ -1,14 +1,13 @@
-
 let uploadField = document.getElementById("image");
 let str, file;
 uploadField.onchange = function () {
     file = this.files[0];
     str = file.name.toLowerCase();
-    if (str.toLowerCase().endsWith('.png') || str.toLowerCase().endsWith('.jpg')|| str.toLowerCase().endsWith('.jpeg')|| str.toLowerCase().endsWith('.jfif')) {
+    if (str.toLowerCase().endsWith('.png') || str.toLowerCase().endsWith('.jpg') || str.toLowerCase().endsWith('.jpeg') || str.toLowerCase().endsWith('.jfif')) {
         if (file.size > 5000000) {
             alert("Fayl xajmi 5mb katta, iltmos kichikroq hajimli rasm kiriting!!!");
             this.value = "";
-        }else{
+        } else {
             return;
         }
     } else {
